8d95f24c03adc28135a66a440af7c96fc396738e
1mmense <turuntaev1997@gmail.com>
2017-12-08 07:53:05 +0400

    api?

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-	resources :posts, path: '/api/posts' do
-		#resources :comments, :only => [:create, :destroy]
-		resources :comments, path: '/api/comments'
-		#root to: "posts#index"
-	end
+    resources :posts, path: '/api/posts' do
+        #resources :comments, :only => [:create, :destroy]
+        resources :comments, path: '/api/comments'
+        #root to: "api/posts#show"
+    end
 end
